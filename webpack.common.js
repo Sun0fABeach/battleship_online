46423acf92d973c718b1239cce7df2c18e65ef2a
Markus Appel <markospoparkos@gmail.com>
2019-04-05 21:08:46 +0200

    Update deps

--- webpack.common.js
+++ webpack.common.js
@@ -80,7 +80,7 @@ module.exports = {
         }
     },
     plugins: [
-        new CleanWebpackPlugin(['dist/*']),
+        new CleanWebpackPlugin(),
         new CopyWebpackPlugin([{from: 'robots.txt'}]),
 
         new webpack.ProvidePlugin({
